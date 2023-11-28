ae65786f0a9f9e9aedf7a9cf6e22e27009e940ac
levik <zhenya12@gmail.com>
2023-11-28 17:12:51 +0200

    small change for demo project.

--- src/main/java/com/levik/bringplayground/BringPlaygroundApplication.java
+++ src/main/java/com/levik/bringplayground/BringPlaygroundApplication.java
@@ -3,13 +3,15 @@ package com.levik.bringplayground;
 import com.bobocode.bring.core.context.impl.BringApplicationContext;
 import com.bobocode.bring.web.BringWebApplication;
 import com.levik.bringplayground.feature.di.Barista;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 public class BringPlaygroundApplication {
 
 	public static void main(String[] args) {
-		BringApplicationContext bringApplicationContext = BringWebApplication.run("com.levik.bringplayground", "com.levik.bringplayground.web");
+		BringApplicationContext bringApplicationContext = BringWebApplication.run(BringPlaygroundApplication.class);
 		Barista barista = bringApplicationContext.getBean(Barista.class);
-		System.out.println(barista.prepareDrink());
+		log.info("Barista is {}", barista.prepareDrink());
 	}
 
 }
--- src/main/java/com/levik/bringplayground/web/ShortenController.java
+++ src/main/java/com/levik/bringplayground/web/ShortenController.java
@@ -2,16 +2,23 @@ package com.levik.bringplayground.web;
 
 import com.bobocode.bring.web.servlet.BringServlet;
 import com.bobocode.bring.web.servlet.annotation.*;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.levik.bringplayground.web.dto.UserRequest;
 import com.levik.bringplayground.web.dto.UserResponse;
+import lombok.AllArgsConstructor;
+import lombok.SneakyThrows;
 
 
+@AllArgsConstructor
 @RequestMapping(path = "/api")
 @RestController
 public class ShortenController implements BringServlet {
 
+    private final ObjectMapper objectMapper;
+
+    @SneakyThrows
     @PostMapping(path = "/shorten")
-    public UserResponse shorten(@RequestBody UserRequest userRequest) {
-        return new UserResponse(userRequest.getOriginalUrl());
+    public String shorten(@RequestBody UserRequest userRequest) {
+        return objectMapper.writeValueAsString(new UserResponse(userRequest.getOriginalUrl()));
     }
 }
